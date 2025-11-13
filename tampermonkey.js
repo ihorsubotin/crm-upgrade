e26e0f48c9701275d2045e3c14223e5c5dcd1abc
ihorsubotin <igor.sybbotin.2002@gmail.com>
2025-11-13 19:40:37 +0200

    Fixed uneven requests

--- tampermonkey.js
+++ tampermonkey.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         CRM automation scripts
 // @namespace    http://tampermonkey.net/
-// @version      2025-11-13
+// @version      2025-11-14
 // @description  try to take over the world!
 // @author       Ihor
 // @include      https://perevodi.keepincrm.com/*
@@ -259,7 +259,7 @@ function checkSupplyOrder(){
 			storedTotal = JSON.parse(oldItems).reduce((sum, item) => sum + (item.amount * item.cost), 0);
 		}catch(e){}
 		let shouldSend = true;
-		if(storedTotal == total){
+		if(Math.abs(storedTotal - total) > 0.1){
 			if(checkSupply > 0){
 				checkSupply--;
 				shouldSend = false;
@@ -321,7 +321,7 @@ async function applySupplyAmountChange(pageId){
 					cost_amount: item.cost_amount,
 					cost_currency: "UAH",
 					cost: item.cost_amount
-				})
+				});
 			}
 		}
 	}
